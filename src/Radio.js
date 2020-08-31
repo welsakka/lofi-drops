1eedc4564cc203759a5217593a30b193888a4344
Waleed <waleedpahome@live.com>
2020-08-30 20:01:22 -0400

    implemented tabs logic

--- src/Radio.js
+++ src/Radio.js
@@ -40,23 +40,26 @@ class Radio extends React.Component {
             ],
             activeChannel : 0
         }
-        this.changeStation = this.changeStation.bind(this);
+        this.changeChannel = this.changeChannel.bind(this);
     }
 
 
-    //New idea: Just change the state of the radio component to allow users to switch between channels
-    //Figure out how to integrate tabs into the Radio and channel dynamic. Perhaps just define tabs in the Radio component
-    changeStation(i) {
+    //Method that takes in array index number to identify which channel to tune into. Logic taken from :
+    // https://stackoverflow.com/questions/43638938/updating-an-object-with-setstate-in-react/43639228
+    // Returns an array of objects
+    changeChannel(i) {
         this.setState(prevState => {
             return {
                 channels : prevState.channels.map( (item, index) => 
                     index === i ? { ...item, isTunedIn : true, isMuted : false} : {...item, isTunedIn : false, isMuted : true}
-                )
+                )                
             }
         })
     }
 
-    //<SimpleTabs channels={this.state.channels} /> Return this back to header
+    // <button className="chillLofi" onClick={i => this.changeStation(0)}>Chill Lofi</button>
+    // <button className="animeStyle" onClick={i => this.changeStation(1)}>Anime Style</button>
+    // <button className="jazzHop" onClick={i => this.changeStation(2)}>Jazz Hop</button>
     render() {
         return (
             <div className="radio">
@@ -65,9 +68,11 @@ class Radio extends React.Component {
                     <Channel station={this.state.channels[1]} />
                     <Channel station={this.state.channels[2]} />            
                 </header>
-                <button className="chillLofi" onClick={this.changeStation(0)}>Chill Lofi</button>
-                <button className="animeStyle" onClick={this.changeStation(1)}>Anime Style</button>
-                <button className="jazzHop" onClick={this.changeStation(2)}>Jazz Hop</button>
+                <SimpleTabs onChannelChange0={() => this.changeChannel(0)}
+                            onChannelChange1={() => this.changeChannel(1)}
+                            onChannelChange2={() => this.changeChannel(2)}
+                            state={this.state} />
+
             </div>
         )
     }
--- src/Tabs.js
+++ src/Tabs.js
@@ -7,6 +7,43 @@ import Tab from '@material-ui/core/Tab';
 import Channel from './Channel.js'
 import Radio from './Radio.js'
 
+/*
+class SimpleTabs extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = this.props.state; 
+    this.changeStationTabs = this.changeStationTabs.bind(this);
+  }
+
+  changeStationTabs(e) {
+    e.preventDefault();
+
+    const {station} = this.state;
+    const {changeStation} = this.props.func;
+
+    changeStation(station);
+
+  }
+
+  render() {
+    return (
+      <div className="tabs">
+        <AppBar position="static">
+          <Tabs value={value} onChange={i => changeStationTabs(i)}>
+            <Tab label= {this.props.channels[0].name} />
+            <Tab label= {this.props.channels[1].name} />
+            <Tab label= {this.props.channels[2].name} />
+          </Tabs>
+        </AppBar>
+        {value === 0 && }
+        {value === 1 && i => {changeStationTabs(0)}}
+        {value === 2 && i => {changeStationTabs(0)}}
+      </div>
+    )
+  }
+}
+*/
+
 //This react component creates the tabs to select channels and handles the logic of switching channels
 export default function SimpleTabs(props) {
   const [value, setValue] = React.useState(0);
@@ -15,30 +52,17 @@ export default function SimpleTabs(props) {
     setValue(newValue);
   };
 
-  const changeStation = (i) => {
-    return (
-      <Channel
-          station={{
-              name: props.channels[i].name,
-              source: props.channels[i].src,
-              gif: props.channels[i].gif
-          }}
-      />
-    )
-  }
 
   return (
     <div className="tabs">
       <AppBar position="static">
         <Tabs value={value} onChange={handleChange}>
-          <Tab label= {props.channels[0].name} />
-          <Tab label= {props.channels[1].name} />
-          <Tab label= {props.channels[2].name} />
+          <Tab label={props.state.channels[0].name} onClick={props.onChannelChange0} />
+          <Tab label={props.state.channels[1].name} onClick={props.onChannelChange1}/>
+          <Tab label={props.state.channels[2].name} onClick={props.onChannelChange2}/>
         </Tabs>
       </AppBar>
-      {value === 0 && changeStation(0)}
-      {value === 1 && changeStation(1)}
-      {value === 2 && changeStation(2)}
     </div>
   );
 }
+//export default SimpleTabs
